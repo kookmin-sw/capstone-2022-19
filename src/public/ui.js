166da5a4e99e2edd7ff73bf323bc514b07d5e15d
Wooseok Jang <reverse@kookmin.ac.kr>
2022-05-18 18:45:49 +0900

    fix runInitialTest

--- src/public/ui.js
+++ src/public/ui.js
@@ -4,6 +4,9 @@ let i= -1;
 let isStop = false;
 let playing ;
 let timeValue;
+let retValue;
+let start = new Date();
+let end = new Date();
     async function runModel(){
     model = await faceLandmarksDetection.load(faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
     }
@@ -16,7 +19,6 @@ let timeValue;
             returnTensors: false,
             flipHorizontal: false,
             predictIrises: true
-
         });
     
         facePoint(face);
@@ -25,50 +27,47 @@ let timeValue;
     
     }
     async function initialTest(localvideo){
-        const face =  await model.estimateFaces({
+        const face = await model.estimateFaces({
             input:localvideo,
             returnTensors: false,
             flipHorizontal: false,
             predictIrises: true
         });
-         let value=await verification(face);
-         return value
+         let value = await verification(face);
+
     }
     
         
     const runcheckscore = (localvideo, roomId) => {
-     
-        setInterval(() => {checkscore(localvideo, roomId);}, 100);
+        setInterval(() => {checkscore(localvideo, roomId);}, 1000);
     }
-
+    
     async function runInitialTest(localvideo){
-  
-        let time = 1;
-      
+        let j = 0;
         timeValue= setInterval(() => {
-                console.log(time);
-                initialTest(localvideo);
-                time= time -1;
-                if (time <=0){
+                if(j == 50){
                     clearInterval(timeValue);
-                    let retValue = returnValue();
+                    retValue = returnValue();
                     console.log(retValue);
-                    console.log(retValue[i=i+1]);
-                    console.log(retValue[i=i+1]);
-                  
-                }
-                
-        }, 100);
-    }
+                    zeroSet();
+                 }
+                 else{
+                    initialTest(localvideo);
+                    j++;
+                 }
+                 console.log(j);
+                  }, 100);
 
-    /*
-    localvideo.onloadeddata = function(){
-        //runcheckscore(localvideo, tmp);
-        runInitialTest(localvideo, tmp);
-        setTimeout(function(){
-            runcheckscore(localvideo, tmp)
-        }, 20000);
     }
-    */
+    
+
+
+    localvideo.onloadeddata = function(){
+        runcheckscore(localvideo, rId);
+        //runInitialTest(localvideo, tmp);
+        setTimeout(function(){
+            runcheckscore(localvideo, rId)
+        }, 5000);
+    }
 
 
