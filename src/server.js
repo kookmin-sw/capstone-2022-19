3be64d28ee5abce0f2f238ae87b4a88466db64a4
HCH96 <sjrnfl0412@kookmin.ac.kr>
2022-05-18 16:48:16 +0900

    방 구현, 이름 표시

--- src/server.js
+++ src/server.js
@@ -42,43 +42,55 @@ app.use(session({
 app.use("/", routing);
 
 
-
+let room_chk = {};
 let rooms = {};
+let room_manager = {};
 
 
 //socket code
 io.on("connection", (socket) => {
     console.log(`${socket.id} Connected`);
 
+    socket.on("totalScore" ,(data)=> {
+        console.log(data);
+    })
+
     //교수 입장
     socket.on("professorJoin", (data) => {
         let roomId = data.roomId;
         let userId = data.userId;
-        console.log(`professor : ${userId} joined `);
-        video_load = { roomId: roomId, userId: socket.id };
-        if (roomId in rooms) {
+        console.log(`professor : ${userId} joined ${roomId} `);
+
+        if (room_chk[roomId] === true) {
             console.log("This room already exist");
             socket.emit("alreadyExist");
-        } else {
-            rooms[roomId] = { presenter: [], viewer: userId };
+        } else{
+            rooms[roomId] = {};
+            room_chk[roomId] = true;
+            room_manager[roomId] = socket.id;
             socket.join(roomId);
             socket.emit('createRoom', data);
         }
         console.log(rooms[roomId]);
     });
 
+
+
+    //학생 입장
     socket.on("studentJoin", (data) => {
         let roomId = data.roomId;
         let userId = data.userId;
-        console.log(`Student : ${userId} joined `);
+        let userName = data.userName;
 
-        if (roomId in rooms) {
-            data['index'] = rooms[roomId].presenter.length;
-            rooms[roomId].presenter.push(userId);
+        console.log(`userName : ${data.userName}`);
+        console.log(`Student : ${userName} (${userId}) joined `);
+
+        if (room_chk[roomId] === true) {
+            rooms[roomId][userId] = {username : userName};
             socket.join(roomId);
             socket.emit('joinRoom', data);
 
-        } else {
+        } else{
             console.log("This room doesn't exist");
             socket.emit("noRoom");
         }
@@ -86,16 +98,20 @@ io.on("connection", (socket) => {
         console.log(rooms[roomId]);
     })
 
+    //학생 >> 교수
     socket.on("studentSendIce", (candidate, data) => {
-        socket.to(rooms[data.roomId].viewer).emit("stuIceArrived", candidate, data);
-    })
-
-    socket.on("professorSendIce", (candidate, data) => {
-        socket.to(data.userId).emit("proIceArrived", candidate, data);
+        console.log(` room manager : ${room_manager[data.roomId]}`);
+        socket.to(room_manager[data.roomId]).emit("stuIceArrived", candidate, data);
     })
 
     socket.on("reqAnswer", (offer, data) => {
-        socket.to(rooms[data.roomId].viewer).emit("reqAnswer", offer, data);
+        socket.to(room_manager[data.roomId]).emit("reqAnswer", offer, data);
+    })
+    
+    // 교수 >> 학생
+    socket.on("professorSendIce", (candidate, data) => {
+        console.log(`professor send ICE to ${data.userId}`);
+        socket.to(data.userId).emit("proIceArrived", candidate, data);
     })
 
     socket.on("sendAnswer", (answer, data) => {
