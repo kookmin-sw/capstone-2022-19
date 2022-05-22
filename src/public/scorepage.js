d9bf70c14cf6aa6d2ab271db73e380b8cbe8362e
Wooseok Jang <reverse@kookmin.ac.kr>
2022-05-22 12:29:10 +0900

    Add to heroku

---
+++ src/public/scorepage.js
@@ -0,0 +1,13 @@
+function scorepage(){
+    storage[0] =  (8 + storage[0])/2;
+   // console.log(storage[0]);
+    storage[3] = (17*0.9 + storage[3]*0.1)/2;
+    //console.log(storage[3]);
+    storage[4] = (5 + storage[4])/2;
+    //console.log(storage[4]);
+    storage[6] = (-5 + storage[6])/2;
+    //console.log(storage[6]);
+    runcheckscore(localvideo);
+    
+
+}
